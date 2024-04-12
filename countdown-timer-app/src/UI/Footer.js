77ff3090d17ddf0867fee3fa73ba93886968e130
Harrison-Holt <hholt2901@gmail.com>
2024-04-12 12:11:52 -0400

    added styling for the footer icons

--- countdown-timer-app/src/UI/Footer.js
+++ countdown-timer-app/src/UI/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
+import './footer.css';
 
 function Footer() {
-
     const footerStyles = {
         display: 'flex',
         alignItems: 'center',
@@ -36,24 +36,20 @@ function Footer() {
     return (
         <footer style={footerStyles}>
             <h1 style={headerStyle}>Let's Connect!</h1>
-            <a href="https://github.com/Harrison-Holt" style={iconStyle} aria-label="Visit Harrison's GitHub profile" title="Visit Harrison's GitHub profile">
+            <a href="https://github.com/Harrison-Holt" className="icon-link" aria-label="Visit Harrison's GitHub profile" title="Visit Harrison's GitHub profile">
                 <i className="fab fa-github"></i>
             </a>
-            <a href="https://www.linkedin.com/in/harrison-holt-18a703202" style={iconStyle} aria-label="Visit Harrison's LinkedIn profile" title="Visit Harrison's LinkedIn profile">
+            <a href="https://www.linkedin.com/in/harrison-holt-18a703202" className="icon-link" aria-label="Visit Harrison's LinkedIn profile" title="Visit Harrison's LinkedIn profile">
                 <i className="fab fa-linkedin"></i>
             </a>
-            <a href="mailto:hholt2901@gmail.com" style={emailIconStyle} aria-label="Send an email to Harrison" title="Send an email to Harrison">
+            <a href="mailto:hholt2901@gmail.com" className="icon-link email-icon" aria-label="Send an email to Harrison" title="Send an email to Harrison">
                 <i className="fa fa-envelope"></i>
             </a>
         </footer>
-
-
     );
-
-   
-
 }
 
 export default Footer;
+
 
 
