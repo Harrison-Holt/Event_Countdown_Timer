1fd727eff686a6f84fdb84b5ac0bbb6296bd7b26
hsholt <hsholt@clemson.edu>
2024-09-16 14:20:04 -0400

    fixed files

--- countdown-timer-app/src/App.js
+++ countdown-timer-app/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './UI/Header'; 
 import Footer from './UI/Footer'; 
 import Counter from './Page/Counter'; 
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 function App() {
   return (
