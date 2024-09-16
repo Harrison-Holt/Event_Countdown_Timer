791cb5941fc322082fb3723693ea37e8be167e83
hsholt <hsholt@clemson.edu>
2024-09-16 13:40:00 -0400

    fixed files

--- countdown-timer-app/src/App.js
+++ countdown-timer-app/src/App.js
@@ -2,29 +2,17 @@ import React from 'react';
 import Header from './UI/Header'; 
 import Footer from './UI/Footer'; 
 import Counter from './Page/Counter'; 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Row, Col } from 'react-bootstrap'; 
 
 function App() {
   return (
-    <Container fluid>
-      <Row>
-        <Col>
+    <div>
       <Header />
-        </Col>
-      </Row>
-      <Row>
-        <Col>
+      <main>
         <Counter />
-        </Col>
-      </Row>
-      <Row>
-        <Col>
+      </main>
       <Footer />
-      </Col>
-      </Row>
-      </Container>  
-    );
+    </div>
+  );
 }
 
 export default App;
