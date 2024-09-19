956dbb3d23103c21970002265840a5d9070c993d
hsholt <hsholt@clemson.edu>
2024-09-19 11:03:30 -0400

    Changed the portfolio homepage button

--- countdown-timer-app/src/UI/Header.js
+++ countdown-timer-app/src/UI/Header.js
@@ -7,7 +7,7 @@ function Header() {
         <Row as="header">
             <Col className="d-flex flex-row justify-content-between align-items-center p-3">
             <h1 className="ms-3">Event Countdown Timer</h1>
-            <a href="https://harrisonholt.dev/" className="btn btn-outline-secondary">portfolio homepage</a>
+            <a href="https://harrisonholt.dev/" className="btn btn-secondary">portfolio homepage</a>
             </Col>
         </Row>
     ); 
